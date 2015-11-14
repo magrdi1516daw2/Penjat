fe73890809c3a3bf565203137f1eb827a12944cb
magradi1516daw2 <magradi1516daw2@gmail.com>
2015-11-15 00:02:15 +0100

    Versió amb la classe Partida del projecte Penjat. Problemes: no es veu l'array lletres y la pàgina s'actualitza amb cada "envia" perdent la informació i canviant la paraula.

--- joc.php
+++ joc.php
@@ -18,27 +18,23 @@ include 'Partida.php';
 <head><title>Proves Penjat</title></head>
 
 <body>
-		<?php 
-	
-	$partida->mostraHidden(); 
-   echo 'S\'ha cridat a mostrarHidden()'; 
-$partida->guardarLletra(); 
-     echo 'S\'ha cridat a guardLletra()'; 
-	$partida->comprovarLletra(); 
-echo 'S\'ha cridat a comprovarLletra()'; 
-			
-     ?> 
-	
+    
+    <?php $partida->mostraHidden(); 
+    echo 'S\'ha cridat a mostrarHidden()'; 
+    $partida->guardarLletra(); 
+    echo 'S\'ha cridat a guardLletra()'; 
+    $partida->comprovarLletra(); 
+    echo 'S\'ha cridat a comprovarLletra()'; 
+    ?> 
 
-	
-	<div id="joc">
-		<form method="GET">
-			lletra: 
-			<input type="text" name="lletra" maxlength="1" size="1" >
-			<input type="submit" value="Envia"/>
-		<!-- <input type="submit" name="reset" value="Nou Joc"/> -->
-		</form>
-	</div>
+    <div id="joc">
+	<form method="GET">
+            lletra: 
+            <input type="text" name="lletra" maxlength="1" size="1" >
+            <input type="submit" value="Envia"/>
+            <!-- <input type="submit" name="reset" value="Nou Joc"/> -->
+	</form>
+    </div>
 </body>
 </html>
 
