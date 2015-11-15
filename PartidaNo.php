aa937b6d5d51a6c59cb2128b2745477bd26b4500
magradi1516daw2 <magradi1516daw2@gmail.com>
2015-11-15 18:18:59 +0100

    Versió sense OBJECTES del projecte Penjat. FALTA: CSS, puntuacions , ranking.

--- PartidaNo.php
+++ PartidaNo.php
@@ -1,4 +1,6 @@
-<?php    	
+<?php 
+
+    	
 	function buscarParaula(){
 		$arxiu = fopen('paraules.txt','r') or die ('error lectura');
 		$i=0;
@@ -16,7 +18,8 @@
 			$_SESSION['hidden'][$i] = ' _ ';
 		}
 	return $_SESSION['paraula'];
-        }
+		
+	}
 	
 	function creaHidden(){
 		for($i=0;$i<count($_SESSION['paraula']);$i++){
@@ -26,11 +29,11 @@
 	} 
 	
 	function guardarLletra(){
-		$_SESSION['lletra'] =  strtolower($_SESSION['lletra']);
+		$_SESSION['lletra'] =  strtolower($_GET["lletra"]);
 		if(ctype_alpha($_SESSION['lletra'])){
-		array_push($_SESSION['lletres'], $_SESSION['lletra']);
-		if(count(array_unique($_SESSION['lletres']))<count($_SESSION['lletres'])){
-			// Array has duplicates
+			array_push($_SESSION['lletres'], $_SESSION['lletra']);
+			if(count(array_unique($_SESSION['lletres']))<count($_SESSION['lletres'])){
+			// Array té duplicats
 			$_SESSION['lletres'] = array_unique($_SESSION['lletres']);
 			$_SESSION['repeticio'] = 'Lletra '.$_SESSION['lletra'].' ja dita.</br>';
 		}
@@ -39,62 +42,64 @@
 	}
 	
 	function missatge(){
-            $_SESSION['missatge']='';
-            if($_SESSION['hidden'] == $_SESSION['paraula'] && $_SESSION['intents']<6){
+		unset($_SESSION['missatge']);
+		if($_SESSION['hidden'] == $_SESSION['paraula'] && $_SESSION['intents']<6){
 		$_SESSION['missatge'] = '</br>Has guanyat!';
-            }
-            else{
-		if($_SESSION['intents']>=6){
-		$_SESSION['missatge'] = '</br>Has perdut... La paraula era '.implode($_SESSION['paraula']);
-                }
-            }
-	return $_SESSION['missatge'];
-        }
-	
-        function esLletra(){
-            if(!empty($_GET["lletra"]) && !ctype_alpha($_GET["lletra"])){
-            	echo '<meta http-equiv="refresh" content="1; url=error.php" />';
-            }
-	}	
-		
-	function comprovarLletra() { 
-            $_SESSION['lletra'] =  strtolower($_SESSION['lletra']);
-            $_SESSION['error']=0;
-            if(ctype_alpha($_SESSION['lletra'])){
-		for($i=0;$i<count($_SESSION['paraula']);$i++){
-			if($_SESSION['lletra'] == $_SESSION['paraula'][$i]){
-	//si coincideix mira si ja esta posada al seu lloc a la paraula amagada
-                		$_SESSION['hidden'][$i] = $_SESSION['lletra'];
-				$_SESSION['error']++;
-			}else{
-				if($_SESSION['hidden'][$i] != $_SESSION['paraula'][$i]){
-                    		$_SESSION['hidden'][$i] = ' _ ';
-                        	} 
+		}
+		else{
+			if($_SESSION['intents']>=6){
+			$_SESSION['missatge'] = '</br>Has perdut... La paraula era '.implode($_SESSION['paraula']);
 			}
 		}
-	
-		if(!$_SESSION['error']){
-			$_SESSION['intents']++;
+	return $_SESSION['missatge'];
+}
+		function esLletra(){
+			if(!empty($_GET["lletra"]) && !ctype_alpha($_GET["lletra"])){
+				echo '<meta http-equiv="refresh" content="1; url=error.php" />';
+			}
+			
+		}	
+		
+	function comprovarLletra() { 
+		$_SESSION['lletra'] =  strtolower($_GET["lletra"]);
+		$_SESSION['error']=0;
+		if(ctype_alpha($_SESSION['lletra'])){
+			for($i=0;$i<count($_SESSION['paraula']);$i++){
+				if($_SESSION['lletra'] == $_SESSION['paraula'][$i]){
+					//si coincideix mira si ja esta posada al seu lloc a la paraula amagada
+					$_SESSION['hidden'][$i] = $_SESSION['lletra'];
+					$_SESSION['error']++;
+				}else{
+					if($_SESSION['hidden'][$i] != $_SESSION['paraula'][$i]){
+					$_SESSION['hidden'][$i] = ' _ ';
+					} 
+				}
+			}
+			if(!$_SESSION['error']){
+				$_SESSION['intents']++;
+			}
 		}
-            }
 	return  $_SESSION['hidden'];
 	}
 
+	
 	function boto(){
-            if(!empty($_SESSION['missatge'])){
-		echo '<input type="submit" value="Envia" disabled/>';
-            }else{
-		echo '<input type="submit" value="Envia"/>';
-            }
+		if(!empty($_SESSION['missatge']) || $_SESSION['intents']>=6){
+			echo '<input type="submit" value="Envia" disabled/>';
+		}else{
+			echo '<input type="submit" value="Envia"/>';
+		}
 	}
 	
 	function imatge(){
-            if(!isset($_SESSION['intents'])){
-		echo '<img id="imatge" src="images/ahorcado0.png">';
-            }
-            else{
-		echo '<img id="imatge" src="images/ahorcado'.$_SESSION['intents'].'.png">';
-            }
+		if(!isset($_SESSION['intents'])){
+			echo '<img id="imatge" src="images/ahorcado0.png">';
+		}
+		else{
+			echo '<img id="imatge" src="images/ahorcado'.$_SESSION['intents'].'.png">';
+		}
+
 	}
+
 
 ?>
