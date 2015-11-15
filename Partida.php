0a8f65fa10d6e0963e088c66e9e1ebe33e2b708f
magradi1516daw2 <magradi1516daw2@gmail.com>
2015-11-15 04:26:40 +0100

    Versió sense OBJECTES del projecte Penjat. FALTA: CSS, puntuacions , ranking i autenticació d'usuaris.

--- Partida.php
+++ Partida.php
@@ -1,19 +1,7 @@
 <?php 
-class Partida {
-	public $hidden;
-	public $paraula;
-	public $intents;
-	public $lletra;
-	public $lletres;
-        
-	function __construct() {
-           $this->buscarParaula();
-            $this->intents = 0;
-            
-                    }
 
-        
- 	public function buscarParaula(){
+    	
+	function buscarParaula(){
 		$arxiu = fopen('paraules.txt','r') or die ('error lectura');
 		$i=0;
 		while(!feof($arxiu)){
@@ -21,47 +9,109 @@ class Partida {
 			$i++;
 		}
 		array_pop($linies);
-		$this->paraula = $linies[rand(0,count($linies)-1)];
+		$_SESSION['paraula'] = $linies[rand(0,count($linies)-1)];
 		fclose($arxiu);
+		$_SESSION['paraula'] = str_split($_SESSION['paraula']);
+		array_pop($_SESSION['paraula']);
 		//crea hidden a partir de la paraula
-  		for($i=0;$i<strlen($this->paraula);$i++){
-			$this->hidden[$i] = ' _ ';
-		} 
-		array_pop($this->hidden);  
-		$this->paraula = str_split($this->paraula);
-		echo implode($this->paraula);
- 
- }
- 
-	public function mostraHidden(){
-	 	echo '</br>';
-	 	echo implode($this->hidden);
-		echo '</br>';
-		}  
-	
-	public function guardarLletra(){
-		if(ctype_alpha($this->lletra)){
-			array_push($this->lletres,  $this->lletra); 
-			echo implode($this->lletres);
-			echo '</br>'; 
-		}	
+  		for($i=0;$i<count($_SESSION['paraula']);$i++){
+			$_SESSION['hidden'][$i] = ' _ ';
+		}
+	return $_SESSION['paraula'];
+		
 	}
 	
-	public function comprovarLletra(){
-		if(ctype_alpha($this->lletra)){
-			for($i=0;$i<count($this->paraula);$i++){
-				if($this->lletra ==  $this->paraula[$i]){
-					//si coincideix mira si ja esta posada al seu lloc a la paraula amagada
-					$this->hidden[$i] =$this->lletra;
-				}else{
-					if($this->hidden[$i] != $this->paraula[$i]){
-						$this->hidden[$i] = ' _ ';
-					}
-				}
-			}	
-		echo $this->intents++; //ha d sumar nomes quan la lletra es erronia
+	function creaHidden(){
+		for($i=0;$i<count($_SESSION['paraula']);$i++){
+			$_SESSION['hidden'][$i] = ' _ ';
+		}  
+	return $_SESSION['hidden'];
+	} 
+	
+	function guardarLletra(){
+		$_GET["lletra"] =  strtolower($_GET["lletra"]);
+		array_push($_SESSION['lletres'], $_GET["lletra"]);
+		if(count(array_unique($_SESSION['lletres']))<count($_SESSION['lletres'])){
+			// Array has duplicates
+			$_SESSION['lletres'] = array_unique($_SESSION['lletres']);
+			$_SESSION['repeticio'] = 'Lletra '.$_GET["lletra"].' ja dita.</br>';
+		}
+	return $_SESSION['lletres'];
+	}
+	
+	function missatge(){
+		if($_SESSION['hidden'] == $_SESSION['paraula'] && $_SESSION['intents']<6){
+		$_SESSION['missatge'] = '</br>Has guanyat!';
+		}
+		else{
+			if($_SESSION['intents']>=6){
+			$_SESSION['missatge'] = '</br>Has perdut... La paraula era '.implode($_SESSION['paraula']);
+			}
+		}
+	return $_SESSION['missatge'];
+}
+		function esLletra(){
+			if(!ctype_alpha($_GET["lletra"])){
+				return '<meta http-equiv="refresh" content="0; url=error.php" />';
+			}
+		}	
+		
+	function comprovarLletra() { 
+		$_GET["lletra"] =  strtolower($_GET["lletra"]);
+		$_SESSION['error']=0;
+		for($i=0;$i<count($_SESSION['paraula']);$i++){
+			if($_GET["lletra"] == $_SESSION['paraula'][$i]){
+				//si coincideix mira si ja esta posada al seu lloc a la paraula amagada
+				$_SESSION['hidden'][$i] = $_GET["lletra"];
+				$_SESSION['error']++;
+			}else{
+				if($_SESSION['hidden'][$i] != $_SESSION['paraula'][$i]){
+					$_SESSION['hidden'][$i] = ' _ ';
+				} 
+				
+			}
+		}
+		if(!$_SESSION['error']){
+			$_SESSION['intents']++;
+		}
+	return  $_SESSION['hidden'];
+	}
+
+	
+	function boto(){
+		if(!empty($_SESSION['missatge'])){
+			echo '<input type="submit" value="Envia" disabled/>';
+		}else{
+			echo '<input type="submit" value="Envia"/>';
 		}
 	}
 	
-}	
+	function imatge(){
+		switch ($_SESSION['intents']) {
+			case 0:
+				echo '<img id="imatge" src="images/ahorcado0.png">';
+				break;
+			case 1:
+				echo '<img id="imatge" src="images/ahorcado1.png">';
+				break;
+			case 2:
+				echo '<img id="imatge" src="images/ahorcado2.png">';
+				break;
+			case 3:
+				echo '<img id="imatge" src="images/ahorcado3.png">';
+				break;
+			case 4:
+				echo '<img id="imatge" src="images/ahorcado4.png">';
+				break;
+			case 5:
+				echo '<img id="imatge" src="images/ahorcado5.png">';
+				break;
+			case 6:
+				echo '<img id="imatge" src="images/ahorcado6.png">';
+				break;
+		}
+	}
+
+
+?>
 
--- joc.php
+++ joc.php
@@ -1,41 +1,85 @@
-<?php
+<?PHP
+  session_start();
+  include 'PartidaNo.php';
 
-if(session_id() === '' || !isset($_SESSION)) {
-    session_start();
-}
-include 'Partida.php';
-	
-  if(!isset($partida)){
-	$partida = new Partida();
-	
+  $_SESSION['reset'] = $_GET["reset"];
+    $_SESSION['lletra'] = $_GET["lletra"];
+    
+//acció del botó nou joc - reseteja les variables de sessio  
+  if(isset($_SESSION['reset'])){
+    session_unset(); 
+    $_SESSION['paraula'] = buscarParaula();
+    $_SESSION['hidden'] = creaHidden();
+     }
+      
+//si no existeix 'paraula' la crea     
+	if(!isset($_SESSION['paraula'])){
+		$_SESSION['paraula'] = buscarParaula();
+		
+		echo implode($_SESSION['paraula']).'<br>';
 	}
-	
-?> 
+	else {
+		echo 'SENSE CANVI: '.implode($_SESSION['paraula']).'<br>';// print_r($_SESSION['paraula']); echo '</br>';
+   }
 
-<!DOCTYPE html>
+//guarda la lletra entrada en un array lletres
+	if(!isset($_SESSION['lletres'])){
+		$_SESSION['lletres'] = array();
+		$_SESSION['lletres'] = guardarLletra();
+	}else{
+		$_SESSION['lletres'] = guardarLletra();
+	}
+	echo 'Lletres dites: '.implode($_SESSION['lletres']);echo '<br>';
+	if(!empty($_SESSION['repeticio'])){
+		echo $_SESSION['repeticio']; 
+		$_SESSION['repeticio'] = '';
+		}
+
+//controlador dels intents/errors  
+	if(!isset($_SESSION['intents']) || $_SESSION['intents']>6){
+	  $_SESSION['intents'] = 0;
+	}
+
+  
+	comprovarLletra();
+			
+ //si no existeix 'hidden' la crea     
+	if(!isset($_SESSION['hidden'])){
+	$_SESSION['hidden'] = array();
+	$_SESSION['hidden'] = creaHidden();
+		}
+
+	
+
+	echo implode($_SESSION['hidden']); echo '</br>';
+	echo 'Intents: '.$_SESSION['intents'];
+?>
+
+
 <html>
-<meta charset="UTF-8"/>
-<head><title>Proves Penjat</title></head>
+
+<head>
+	<?PHP echo esLletra(); ?>
+	<title>Penjat - Jugant</title>
+</head>
 
 <body>
-    
-    <?php $partida->mostraHidden(); 
-    echo 'S\'ha cridat a mostrarHidden()'; 
-    $partida->guardarLletra(); 
-    echo 'S\'ha cridat a guardLletra()'; 
-    $partida->comprovarLletra(); 
-    echo 'S\'ha cridat a comprovarLletra()'; 
-    ?> 
+	<div id="imatge">
+		<?PHP imatge(); ?>
+	</div>
+	
+	<div id="missatge">
+		<?PHP echo missatge(); ?>
+	</div>
+	
+	<div id="formulari">
+		<form action="/jocNo.php" method="GET">
+			Lletra: 
+			<input type="text" name="lletra" maxlength="1" size="1" >
+			<?PHP boto(); ?>
+			<input type="submit" name="reset" value="Nou Joc"/>
+		</form>
+	</div>
 
-    <div id="joc">
-	<form method="GET">
-            lletra: 
-            <input type="text" name="lletra" maxlength="1" size="1" >
-            <input type="submit" value="Envia"/>
-            <!-- <input type="submit" name="reset" value="Nou Joc"/> -->
-	</form>
-    </div>
 </body>
 </html>
-
-
