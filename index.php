13e01d65b3e240e004ffe0a14febd0b5ac57b579
magradi1516daw2 <magradi1516daw2@gmail.com>
2015-11-15 18:16:02 +0100

    Versió sense OBJECTES del projecte Penjat. FALTA: CSS, puntuacions , ranking.

--- index.php
+++ index.php
@@ -1,51 +1,49 @@
 <?php 
 session_start();
-    
-    function guardarUsuari(){
-	$_SESSION['validacio'] = true;
-	if(!isset($_SESSION['nom'])){
-        	if(!empty($_POST["nom"]) || ctype_alpha($_POST["nom"])){
-                	$_SESSION['nom']  = $_POST["nom"];
-		}else{	
-			$_SESSION['validacio'] = false;
-		}
-        }
-	if(!isset($_SESSION['email'])){
-            if(filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
-		$_SESSION['email']  = $_POST["email"];
-            }else{	
-		$_SESSION['validacio'] = false;
-            }
-        }
 
-	if(!isset($_SESSION['contra'])){
-            if(!empty($_POST["password"]) || ctype_alnum( $_POST["password"])){
-		$_SESSION['contra']  = $_POST["password"];
-            }else{	
-		$_SESSION['validacio'] = false;
-            }
-        }
+	function guardarUsuari(){
+		if(!empty($_POST["nom"]) || ctype_alpha($_POST["nom"])){
+			$_SESSION['nom']  = $_POST["nom"];
+			$_SESSION['validacio'] = false;
+		}else{	
+			$_SESSION['validacio'] = true;
+		}
+
+		if(filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+			$_SESSION['email']  = $_POST["email"];
+			$_SESSION['validacio'] = false;
+		}else{	
+			$_SESSION['validacio'] = true;
+		}
+
+		if(!empty($_POST["password"]) || ctype_alnum( $_POST["password"])){
+			$_SESSION['contra']  = $_POST["password"];
+			$_SESSION['validacio'] = false;
+		}else{	
+			$_SESSION['validacio'] = true;
+		}
+		$_SESSIO['puntuacio'] = 0;
 		
-	$_SESSION['punts'] = 0;
-		
-	if(isset($_SESSION['nom']) && isset($_SESSION['email']) && isset($_SESSION['contra']) && !empty($_SESSION['nom'])){
-            $_SESSION['dades'] = $_SESSION['nom'].','.$_SESSION['email'].','.$_SESSION['contra'].','.$_SESSION['punts'];
-            echo $_SESSION['dades'];
-            $_SESSION['usuaris'] = fopen('usuaris.txt','a') or die ('error lectura');
-            fwrite($_SESSION['usuaris'],$_SESSION['dades']);
-            fwrite($_SESSION['usuaris'], PHP_EOL);
-            fclose($_SESSION['usuaris']);
+		if(isset($_SESSION['nom']) && isset($_SESSION['email']) && isset($_SESSION['contra'])){
+			$_SESSION['dades'] = $_SESSION['nom'].','.$_SESSION['email'].','.$_SESSION['contra'].','.$_SESSIO['puntuacio'];
+				echo $_SESSION['dades'];
+			$_SESSION['usuaris'] = fopen('usuaris.txt','a') or die ('error lectura');
+			fwrite($_SESSION['usuaris'],$_SESSION['dades']);
+			fwrite($_SESSION['usuaris'], PHP_EOL);
+			fclose($_SESSION['usuaris']);
+		}
 	}
-    }
 	
-    function validarUsuari(){
-	if($_SESSION['validacio'] && ctype_alpha($_SESSION['nom'])){
-            header('Location:jocNo.php');
-	}
-	else{
-            echo 'Error de validació';
-	}
-    }
+	function validarUsuari(){
+		if(isset($_SESSION['validacio']) && $_SESSION['validacio']){
+		echo 'Error de validació';
+		}
+}
+	
+	function redireccionar(){
+		if(!$_SESSION['validacio']){
+			header('Location:jocNo.php');
+	}}
 
 ?>
 
@@ -56,7 +54,6 @@ session_start();
 </head>
 
 <body>
-
 	<div id="imatge">
 		<img id="imatge" src="images/ahorcado6.png">
 	</div>
@@ -67,13 +64,14 @@ session_start();
 	
 	<div id="formulari">
 		<form action="index.php" method="POST">
-                    Nom: <input type="text" name="nom"> </br>
-                    E-mail: <input type="text" name="email">
-                    <input type="submit" value="JUGAR" value="submit"/></br>
-                    Contrasenya: <input type="password" name="password">
-                    <?php guardarUsuari();
+			Nom: <input type="text" name="nom"></br>
+			E-mail: <input type="text" name="email">
+			<input type="submit" name="envia" value="Jugar"/></br>
+			Contrasenya: <input type="password" name="password">
+			<?php guardarUsuari();
 			validarUsuari();
-                    ?>
+			redireccionar();
+				?>
 
 		</form>
 	</div>
